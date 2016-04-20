c3536d6569fe2a2c2403563fe2f43ce8abcffe80
Doug Klassen <d.p.klassen@gmail.com>
2016-04-20 15:30:19 -0700

    Added VS solution with loadable add-in

---
+++ RevitObjExporter/Commands/Export.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace DougKlassen.Revit.ObjExporter.Commands
+{
+	[Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+	public class Export : IExternalCommand
+	{
+		public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+		{
+			TaskDialog.Show("OBJ Exporter", "running command");
+			//Command code goes here
+
+			return Result.Succeeded;
+		}
+	}
+}
---
+++ RevitObjExporter/StartUp/NewAddIn.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Windows.Media.Imaging;
+using System.Reflection;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace RevitObjExporter.StartUp
+{
+	public static class FileLocations
+	{
+		//AddInDirectory is initialized at runtime
+		public static String AddInDirectory;
+		//AssemblyName is initialized at runtime
+		public static String AssemblyName;
+		public static readonly String imperialTemplateDirectory = @"X:\CAD\_REVIT 2016\Family Templates\English_I\";
+		public static readonly String ResourceNameSpace = @"RevitObjExporter.Resources";
+	}
+
+	public class StartUpApp : IExternalApplication
+	{
+		Result IExternalApplication.OnStartup(UIControlledApplication application)
+		{
+			//initialize AssemblyName using reflection
+			FileLocations.AssemblyName = Assembly.GetExecutingAssembly().GetName().Name;
+			//initialize AddInDirectory. The addin should be stored in a directory named after the assembly
+			FileLocations.AddInDirectory = application.ControlledApplication.AllUsersAddinsLocation + "\\" + FileLocations.AssemblyName + "\\";
+
+			//load image resources
+			BitmapImage largeIcon = GetEmbeddedImageResource("iconLarge.png");
+			BitmapImage smallIcon = GetEmbeddedImageResource("iconSmall.png");
+
+			PushButtonData newCommandPushButtonData = new PushButtonData(
+				 "NewCommandButton", //name of the button
+				 "NewCommand", //text on the button
+				 FileLocations.AddInDirectory + FileLocations.AssemblyName + ".dll",
+				 "RevitObjExporter.Commands.NewCommand");
+			newCommandPushButtonData.LargeImage = largeIcon;
+
+			RibbonPanel newAddInRibbonPanel = application.CreateRibbonPanel("NewAddIn");
+			newAddInRibbonPanel.AddItem(newCommandPushButtonData);
+
+			return Result.Succeeded;
+		}
+
+		Result IExternalApplication.OnShutdown(UIControlledApplication application)
+		{
+			return Result.Succeeded;
+		}
+
+		/// <summary>
+		/// Utility method to retrieve an embedded image resource from the assembly
+		/// </summary>
+		/// <param name="resourceName">The name of the image, corresponding to the filename of the embedded resouce added to the solution</param>
+		/// <returns>The loaded image represented as a BitmapImage</returns>
+		BitmapImage GetEmbeddedImageResource(String resourceName)
+		{
+			Assembly asm = Assembly.GetExecutingAssembly();
+			Stream str = asm.GetManifestResourceStream(FileLocations.ResourceNameSpace + "." + resourceName);
+
+			BitmapImage bmp = new BitmapImage();
+			bmp.BeginInit();
+			bmp.StreamSource = str;
+			bmp.EndInit();
+
+			return bmp;
+		}
+	}
+}
