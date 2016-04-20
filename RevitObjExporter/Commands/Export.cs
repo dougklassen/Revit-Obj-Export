1628fc6e0a723819a7aa4aac601499d8a8a1f06e
Doug Klassen <d.p.klassen@gmail.com>
2016-04-20 16:30:06 -0700

    added code to run obj export

--- RevitObjExporter/Commands/Export.cs
+++ RevitObjExporter/Commands/Export.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 
+using RevitOBJExport;
+
 namespace DougKlassen.Revit.ObjExporter.Commands
 {
 	[Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
@@ -12,8 +14,35 @@ namespace DougKlassen.Revit.ObjExporter.Commands
 	{
 		public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
 		{
-			TaskDialog.Show("OBJ Exporter", "running command");
-			//Command code goes here
+			Document dbDoc = commandData.Application.ActiveUIDocument.Document;
+			View3D exportView = dbDoc.ActiveView as View3D;
+
+			if (exportView == null)
+			{
+				TaskDialog.Show("Error", "Active view must be a 3D view");
+				return Result.Failed;
+			}
+
+			String resultString = Main.ExportView3D(
+				dbDoc, //Revit document
+				exportView,	//View3D to be exported
+				"C:\\objexport\\test.obj",	//file path for export including filename
+				1,	//ouput units, feet = 1
+				1,	//rotation - ProjectNorth = 1, TrueNorth = 2
+				15,	//LOD 0-15 (8 is default)
+				true,	//YZ flipped
+				false,	//group elements
+				false,	//selected elements only
+				true,	//add materials
+				true,	//add textures
+				true,	//replace spaces in material names
+				1,	//texture scale
+				"textures",	//texture directory
+				1,	//label type - by material = 1, by name = 2
+				1,	//label Id position - none = 1, before = 2, after = 3
+				1);	//tag OBJ character = o = 1, g = 2
+
+			TaskDialog.Show("OBJ Export Result", resultString);
 
 			return Result.Succeeded;
 		}
